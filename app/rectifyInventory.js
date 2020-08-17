fc693662e5d340465d5a5a03bc34ef524c0bfab5
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-08-16 20:37:07 -0400

    Created report function

--- app/rectifyInventory.js
+++ app/rectifyInventory.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// const MONGODB_URI = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const db = require('../models');
 
 const generateSingleCollectionReport = async (collectionName) => {
@@ -61,7 +61,7 @@ const checkItemAndInvoiceObject = (items, reportObject) => {
     return reportObject;
 };
 
-const generateCheckBetweenCollections = async (collectionStart, collectionEnd) => {
+const generateCheckBetweenCollectionsObject = async (collectionStart, collectionEnd) => {
     // This is an experment to generalist generateTSACheck to operate in either direction
 
     const uniqueItemNumbers = await db[collectionStart].distinct('itemNumber');
@@ -111,10 +111,210 @@ const generateCheckBetweenCollections = async (collectionStart, collectionEnd) =
         }
     }
 
-    return issueReport
+    return issueReport;
+};
 
+const addStrings = (left, right, operation = '') => {
+    if (operation === '' || operation === 'add') {
+        return Number((Number(left) + Number(right)).toFixed(2));
+    } else if (operation === 'subtract') {
+        return Number((Number(left) - Number(right)).toFixed(2));
+    } else {
+        console.error('Invalid operation provided to addStrings', operation);
+    }
+};
 
-}
+const addToObject = (obj, key, value, operation = '') => {
+    // Checks if obj key exists, either creates or increments
+    if (!obj.hasOwnProperty(key)) {
+        obj[key] = value;
+        return obj;
+    }
+    for (const [valKey, existingValue] of Object.entries(value)) {
+        obj[key][valKey] = addStrings(obj[key][valKey], existingValue, operation);
+    }
+
+    return obj;
+};
+
+const generateArrayUniqueItemsInvoices = (itemsForItemNumber) => {
+    // Gets passed in a list of items, all with the same item number
+    let invoices = {};
+
+    for (let index = 0; index < itemsForItemNumber.length; index++) {
+        const item = itemsForItemNumber[index];
+        let { invoiceNumber, qty, dollarAmount } = item;
+
+        if (!invoiceNumber) {
+            invoiceNumber = 'undefined';
+        }
+
+        invoices = addToObject(invoices, invoiceNumber, { qty, dollarAmount: Number(dollarAmount) });
+        // console.log("generateArrayUniqueItemsInvoices -> invoices", invoices)
+    }
+
+    return invoices;
+};
+
+const onlyUniqueItems = (value, index, self) => self.indexOf(value) === index;
+
+const generateCheckBetweenCollectionsArray = async (collectionStart, collectionEnd) => {
+    // This is an experment to generalist generateTSACheck to operate in either direction
+
+    const uniqueItemNumbers = await db[collectionStart].distinct('itemNumber');
+    // TODO add in optional date range provided from user
+
+    let uniqueItemsAndInvoices = [];
+
+    for (let index = 0; index < uniqueItemNumbers.length; index++) {
+        const itemNumber = uniqueItemNumbers[index];
+        const itemsForItemNumber = await db[collectionStart].find({ itemNumber }); //.distinct('invoiceNumber')
+
+        const invoiceList = { itemNumber, invoices: generateArrayUniqueItemsInvoices(itemsForItemNumber) };
+
+        uniqueItemsAndInvoices.push(invoiceList);
+    }
+
+    // return uniqueItemsAndInvoices;
+    // Comparing to the opposite table now
+
+    for (let index = 0; index < uniqueItemsAndInvoices.length; index++) {
+        const { itemNumber, invoices } = uniqueItemsAndInvoices[index];
+
+        const invoicesList = Object.keys(invoices);
+
+        for (let invoicesIndex = 0; invoicesIndex < invoicesList.length; invoicesIndex++) {
+            const invoiceNumber = invoicesList[invoicesIndex];
+            const matchingItems = await db[collectionEnd].find({ itemNumber, invoiceNumber });
+
+            for (let invInd = 0; invInd < matchingItems.length; invInd++) {
+                const { qty, dollarAmount } = matchingItems[invInd];
+
+                for (const [valKey, existingValue] of Object.entries({ qty, dollarAmount })) {
+                    uniqueItemsAndInvoices[index].invoices[invoiceNumber][valKey] = addStrings(
+                        uniqueItemsAndInvoices[index].invoices[invoiceNumber][valKey],
+                        existingValue,
+                        'subtract'
+                    );
+                }
+                // let { qty, dollarAmount } = uniqueItemsAndInvoices[index].invoices[invoiceNumber];
+
+                // qty = qty - item.qty;
+                // dollarAmount = Number((Number(dollarAmount) - Number(item.dollarAmount)).toFixed(2));
+
+                // uniqueItemsAndInvoices[index].invoices[invoiceNumber] = { qty, dollarAmount };
+            }
+        }
+    }
+
+    // REMOVE EMPTY ITEMS
+    let itemIndicesToRemove = [];
+    for (let index = 0; index < uniqueItemsAndInvoices.length; index++) {
+        const { itemNumber, invoices } = uniqueItemsAndInvoices[index];
+
+        const invoicesList = Object.keys(invoices);
+        for (let invoicesIndex = 0; invoicesIndex < invoicesList.length; invoicesIndex++) {
+            const invoiceNumber = invoicesList[invoicesIndex];
+            const { qty, dollarAmount } = uniqueItemsAndInvoices[index].invoices[invoiceNumber];
+            if (qty === 0 && dollarAmount === 0) {
+                delete uniqueItemsAndInvoices[index].invoices[invoiceNumber];
+            }
+        }
+    }
+
+    uniqueItemsAndInvoices = uniqueItemsAndInvoices.filter((item) => Object.keys(item.invoices) > 0);
+
+    return uniqueItemsAndInvoices;
+};
+
+const reduceTotalQtyDollarAmount = (accumulatedResults, row) => {
+    accumulatedResults.qty = addStrings(accumulatedResults.qty, row.qty);
+    accumulatedResults.dollarAmount = addStrings(accumulatedResults.dollarAmount, row.dollarAmount);
+    return accumulatedResults;
+};
+
+// TODO write check that gathers list of items from each array, and compares items in single loop
+const completeCheck = async (dateStart = new Date(1900, 1, 1, 0, 0, 0, 0), dateEnd = new Date()) => {
+    // generate unique items in tsa and nssdr and combine
+    const uniqueItemsTSA = await db.TSA.distinct('itemNumber');
+    const uniqueItemsNSSDR = await db.NSSDR.distinct('itemNumber');
+    const uniqueItemNumbers = uniqueItemsTSA
+        .concat(uniqueItemsNSSDR)
+        .filter(onlyUniqueItems)
+        .sort((a, b) => Number(a) - Number(b));
+
+    // Now for each item number
+    // I want to check TSA for and rows that include that, and grab the invoice number, qty, date, and dollar amount
+    // Then likewise check NSSDR, grab all of the same for that item number
+    const overviewReportItems = [];
+    const finalReportItems = [];
+    for (let uniqueItemNumberIndex = 0; uniqueItemNumberIndex < uniqueItemNumbers.length; uniqueItemNumberIndex++) {
+        const itemNumber = uniqueItemNumbers[uniqueItemNumberIndex];
+
+        const tsaRows = await db.TSA.find({ itemNumber }).then((results) =>
+            results.map((row) => ({
+                qty: row.qty,
+                dollarAmount: Number(row.dollarAmount),
+                date: row.date,
+                invoiceNumber: row.invoiceNumber || 'undefined',
+            }))
+        );
+        const nssdrRows = await db.NSSDR.find({ itemNumber }).then((results) =>
+            results.map((row) => ({
+                qty: -Number(row.qty),
+                dollarAmount: -Number(row.dollarAmount),
+                date: row.date,
+                invoiceNumber: row.invoiceNumber || 'undefined',
+            }))
+        );
+
+        const combinedRows = tsaRows.concat(nssdrRows).filter((row) => row.date > dateStart && row.date < dateEnd);
+
+        const { qty, dollarAmount } = combinedRows.reduce(reduceTotalQtyDollarAmount, { qty: 0, dollarAmount: 0 });
+        overviewReportItems.push({ itemNumber, qty, dollarAmount });
+
+        // Generating the invoice report
+        let invoices = combinedRows
+            .map((row) => row.invoiceNumber)
+            .filter(onlyUniqueItems)
+            .sort((a, b) => Number(a) - Number(b));
+
+        invoices = invoices.map((invoiceNumber) =>
+            combinedRows
+                .filter((row) => row.invoiceNumber === invoiceNumber)
+                .reduce(reduceTotalQtyDollarAmount, { invoiceNumber, qty: 0, dollarAmount: 0 })
+        );
+
+        finalReportItems.push({ itemNumber, invoices });
+
+        // const placeholderVariableName = combinedRows.reduce((accumulator, row) => {
+
+        //     return accumulator
+        // }, {})
+    }
+
+    const overviewReport = overviewReportItems.filter((item) => item.qty !== 0 && item.dollarAmount !== 0);
+    const totalOverviewOffsets = overviewReport.reduce(reduceTotalQtyDollarAmount, { qty: 0, dollarAmount: 0 });
+
+    const finalReport = finalReportItems
+        .map((item) => ({
+            ...item,
+            invoices: item.invoices.filter((invoice) => invoice.qty !== 0 && invoice.dollarAmount !== 0),
+        }))
+        .filter((item) => item.invoices.length > 0);
+
+    return { overviewReport, totalOverviewOffsets, finalReport };
+
+    // console.log("completeCheck -> finalReport", finalReport)
+    // Having that
+    // sum the quantities for each item regardless of invoice number, sum the dollar amounts regardless of invoice.  Create a report agnostic of invoice as an overview, called overviewReport
+    // overviewReport = [{itemNumber: <itemNumber>, qty: <totalQty>, dollarAmount: <totalDollarAmount>}]
+    // ALSO do a reduce and calculate the total dollarAmount offset and total neg and pos offsets for date range
+    // THEN
+
+    // for each invoice, create an object that contains that same complete counter of qty and dollar ammount
+    // finalReport = [{itemNumber: <itemNumber>, invoices: [ { invoiceNumber: <invoiceNumber>, qty: <totalQty>, dollarAmount: <totalDollarAmount> } ] }]
+};
 
 const generateTSACheck = async () => {
     const uniqueItemNumbers = await db.TSA.distinct('itemNumber');
@@ -166,31 +366,48 @@ const generateTSACheck = async () => {
         }
     }
 
-    return issueReport
+    return issueReport;
 };
 
-const rectify = async () => {
-    const collections = ['NSSDR', 'TSA'];
+// const rectify = async () => {
+//     const collections = ['NSSDR', 'TSA'];
 
-    // ! This is the report for verifying the totals regardless of invoice
-    // let reports = await Promise.all(
-    //     collections.map((collectionName) => generateSingleCollectionReport(collectionName))
-    // );
+//     // ! This is the report for verifying the totals regardless of invoice
+//     // let reports = await Promise.all(
+//     //     collections.map((collectionName) => generateSingleCollectionReport(collectionName))
+//     // );
 
-    // console.log(reports);
+//     // console.log(reports);
 
-    // ! Generating a report based on item number as well as invoice
-    // await generateTSACheck();
-    const TSAtoNSSDR = await generateCheckBetweenCollections('TSA', 'NSSDR')
-    console.log("rectify -> TSAtoNSSDR", TSAtoNSSDR)
-    const NSSDRtoTSA = await generateCheckBetweenCollections('NSSDR', 'TSA')
-    console.log("rectify -> NSSDRtoTSA", NSSDRtoTSA)
+//     // ! Generating a report based on item number as well as invoice
+//     // await generateTSACheck();
+//     // const TSAtoNSSDR = await generateCheckBetweenCollectionsObject('TSA', 'NSSDR');
+//     // console.log('rectify -> TSAtoNSSDR', TSAtoNSSDR);
+//     // const NSSDRtoTSA = await generateCheckBetweenCollectionsArray('NSSDR', 'TSA');
+//     // console.log('rectify -> NSSDRtoTSA', NSSDRtoTSA);
 
-    // ! Now, check between those two, see if there are any entries in NSSDRtoTSA that don't have a match in the TSAtoNSSDR list and viceversa
-    // ! Create a final report, showing issues in each category.
-    // TODO add a catch for undefined invoices in NSSDRtoTSA check
+//     // ! Generating report in array format, comparing both collections
+//     // const TSAReport = await generateCheckBetweenCollectionsArray('TSA', 'NSSDR');
+//     // TSAReport.forEach((item) => console.log(item));
 
-    mongoose.connection.close();
-};
+//     // ! Generate complete report
+//     const report = await completeCheck();
+//     console.log(report);
 
-rectify();
+//     // TODO add in function to prettify this data, before sending it to the client.
+//     // TODO make it a nice table format
+
+//     // TSAReport.forEach(line => console.log(line))
+
+//     // ! Now, check between those two, see if there are any entries in NSSDRtoTSA that don't have a match in the TSAtoNSSDR list and viceversa
+//     // ! Create a final report, showing issues in each category.
+//     // TODO add a catch for undefined invoices in NSSDRtoTSA check
+
+//     mongoose.connection.close();
+// };
+
+// rectify();
+
+module.exports = {
+    completeCheck
+}
--- routes/invRectification.js
+++ routes/invRectification.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const processInvInput = require('../app/processInvInput');
+const rectifyInventory = require('../app/rectifyInventory');
 module.exports = (router) => {
     router.post('/api/invRectification', async ({ body }, res) => {
         try {
@@ -12,6 +13,23 @@ module.exports = (router) => {
         } catch (err) {
             console.log(err);
             res.status(500).send({ error: 'Something went wrong with the server' });
+        }
+    });
+    router.get('/api/invRectification/:dateStart/:dateEnd', async ({ params }, res) => {
+        try {
+            const { overviewReport, totalOverviewOffsets, finalReport } = await rectifyInventory.completeCheck(
+                params.dateStart,
+                params.dateEnd
+            );
+            res.status(200).send({
+                message: 'Successfully generated report',
+                overviewReport,
+                totalOverviewOffsets,
+                finalReport,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send({ error: "There was an error generating your report, it's been logged.  Ask James." });
         }
     });
 };
